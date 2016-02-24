4bfd28f7145328d967f5f79bed00d64170125ec7
kevinfan23 <kevinfan23@ucla.edu>
2016-02-23 21:42:08 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+A minimal digital webpage clock using Javascript that display day of the week, date and time in 12-hour system  
+
+Features or requirements include:  
+1. Bootstrap  
+2. Javascript  
+3. The color of the days has a rainbow theme, starting from sunday  
+4. Designed by [Zixuan(Kevin) Fan] (http://kfaninc.com)
+
---
+++ js/main.js
@@ -0,0 +1,76 @@
+function displayTime() {
+	
+	// Time variables
+	var currentTime = new Date();
+	var day = currentTime.getDay();
+	var date = currentTime.getDate();
+	var month = currentTime.getMonth();
+	var year = currentTime.getFullYear();
+	var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+	var days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+	
+	var meridiem = "AM"
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+	setInterval(displayTime, 1000);
+	
+	// Adding zeros to the digits
+	if (seconds < 10) {
+		seconds = '0' + seconds;
+	}
+	
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	
+	if (hours === 0) {
+		hours = 12;
+	}
+	else if (hours < 10) {
+		hours = '0' + hours;
+	}
+	else if (hours > 12) {
+		hours = hours -12;
+		meridiem = "PM";
+	}
+
+	// Change colors according to the day of the week
+	var day_color;
+	switch(day) {
+		case 0:
+			day_color = "rgb(215, 86, 47)";
+			break;
+		case 1:
+			day_color = "rgb(255, 191, 114)";
+			break;
+		case 2:
+			day_color = "rgb(248, 240, 135)";
+			break;
+		case 3:
+			day_color = "rgb(183, 227, 192)";
+			break;
+		case 4:
+			day_color = "rgb(175, 238, 238)";
+			break;
+		case 5:
+			day_color = "rgb(135, 206, 250)";
+			break;
+		case 6:
+			day_color = "rgb(219, 186, 229)";
+			break;
+		default:
+			break;
+	}
+	document.getElementById('day').style.color = day_color;
+
+
+	// Render text
+    var clockDiv = document.getElementById('clock');
+	clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+	var dayDiv = document.getElementById('day');
+	dayDiv.innerText = days[day];
+	var dateDiv = document.getElementById('date');
+	dateDiv.innerText = months[month] + " " + date + ", " + year;
+}
+	displayTime();
